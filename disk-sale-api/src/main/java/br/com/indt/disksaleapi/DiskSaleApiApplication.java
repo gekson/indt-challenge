1ee1bbfb63d7b2cb2b59207e9b46c7cf782a4cfd
Gekson Cinaty <gekson@MacBook-Pro-de-Gekson.local>
2019-11-16 02:39:22 -0300

    initial api structure

---
+++ disk-sale-api/src/main/java/br/com/indt/disksaleapi/DiskSaleApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.indt.disksaleapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiskSaleApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiskSaleApiApplication.class, args);
+	}
+
+}
---
+++ disk-sale-api/src/test/java/br/com/indt/disksaleapi/DiskSaleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.indt.disksaleapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiskSaleApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
